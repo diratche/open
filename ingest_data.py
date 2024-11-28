6a10e3bee973b06847963464ac9ce11c71e8e6d4
diratche <dimitriiratchet@yahoo.fr>
2024-11-28 15:50:59 +0100

    Update ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -14,10 +14,12 @@ except:
 
 address = server + '?apiKey=' + key
 producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=str.encode)
+i = 0
 while True:
   data = requests.get(address)
   if data.status_code == 200:
     producer.send(topic, json.dumps(data.json()))
     #print(f'Ingestion successful at {time.strftime("%H:%M:%S", time.localtime())}')
-    print(data.json())
+    print(i)
+  i += 1
   time.sleep(periodicity)
