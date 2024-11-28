4483b9843440c6a9ba5a5b53be367718c2dfafd4
diratche <dimitriiratchet@yahoo.fr>
2024-11-28 14:59:27 +0100

    Update ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -3,12 +3,19 @@ import requests
 import json
 import time
 
-def ingest(server, key, topic, periodicity=10):
-  address = server + '?apiKey=' + key
-  producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=str.encode)
-  while True:
-    data = requests.get(address)
-    if data.status_code == 200:
-      producer.send(topic, json.dumps(data.json()))
-      print(f'Ingestion successful at {time.strftime("%H:%M:%S", time.localtime())}')
-    time.sleep(periodicity)
+server = sys.argv[1]
+key = sys.argv[2]
+topic = sys.argv[3]
+try:
+  periodicity = sys.argv[4]
+except:
+  periodicity = 10
+
+address = server + '?apiKey=' + key
+producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=str.encode)
+while True:
+  data = requests.get(address)
+  if data.status_code == 200:
+    producer.send(topic, json.dumps(data.json()))
+    print(f'Ingestion successful at {time.strftime("%H:%M:%S", time.localtime())}')
+  time.sleep(periodicity)
