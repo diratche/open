90b88bac096f2e84bdef932bad17e9045c3342e8
diratche <dimitriiratchet@yahoo.fr>
2024-11-28 15:38:15 +0100

    Update stations_activity.py

--- stations_activity.py
+++ stations_activity.py
@@ -1,1 +1,7 @@
+from kafka import KafkaConsumer, KafkaProducer
+
+
+consumer = KafkaConsumer(topic_in, bootstrap_servers='localhost:9092', group_id='app1', value_serializer=str.decode)
+producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=str.encode)
+
 
