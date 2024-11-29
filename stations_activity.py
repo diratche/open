36d55c8b55ba560dae5bd37bdc4ab5bcfb8595a8
diratche <dimitriiratchet@yahoo.fr>
2024-11-29 11:28:47 +0100

    Update stations_activity.py

--- stations_activity.py
+++ stations_activity.py
@@ -1,7 +1,24 @@
 from kafka import KafkaConsumer, KafkaProducer
+import json
+import sys
 
 
-consumer = KafkaConsumer(topic_in, bootstrap_servers='localhost:9092', group_id='app1', value_serializer=str.decode)
-producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=str.encode)
 
+consumer = KafkaConsumer('velib-stations', bootstrap_servers='localhost:9092', group_id='stations_activity', value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
+station_status_list = {}
+
+for message in consumer:
+    station = message.value
+    try:
+        available_bikes = station_status_list[station['name']]['bikes']
+        available_stands = station_status_list[station['name']]['stands']
+        if station['totalStands']['availabilities']['bikes'] != available_bikes or station['totalStands']['availabilities']['stands'] != available_stands:
+            station_status_list[station['name']] = {'bikes': station['totalStands']['availabilities']['bikes'], 'stands': station['totalStands']['availabilities']['stands']}
+            producer.send('stations-status', station)
+    except KeyError:
+        available_bikes = station['totalStands']['availabilities']['bikes']
+        available_stands = station['totalStands']['availabilities']['stands']
+        station_status_list[station['name']] = {'bikes': available_bikes, 'stands': available_stands}
+        producer.send('stations-status', station)
