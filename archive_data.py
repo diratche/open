34db866878fdaf52c3e2e531dc7ef188672966b8
diratche <dimitriiratchet@yahoo.fr>
2024-11-29 14:21:43 +0100

    Update archive_data.py

--- archive_data.py
+++ archive_data.py
@@ -1,1 +1,12 @@
+from kafka import KafkaConsumer
+import json
+import sys
 
+topic_in = sys.argv[1]
+
+consumer = KafkaConsumer(topic_in, bootstrap_servers='localhost:9092', group_id='archive_data', value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+with open('archive.txt', 'w') as archive:
+    for message in consumer:
+        station = message.value
+        print(station, file=archive)
