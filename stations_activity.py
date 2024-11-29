cdb9c4377673d83abea7c84c7053ce58b0a96e1f
diratche <dimitriiratchet@yahoo.fr>
2024-11-29 12:14:26 +0100

    Update stations_activity.py

--- stations_activity.py
+++ stations_activity.py
@@ -8,18 +8,18 @@ topic_out = sys.argv[2]
 consumer = KafkaConsumer(topic_in, bootstrap_servers='localhost:9092', group_id='stations_activity', value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
-station_status_list = {}
+station_status_dic = {}
 
 for message in consumer:
     station = message.value
     try:
-        available_bikes = station_status_list[station['name']]['bikes']
-        available_stands = station_status_list[station['name']]['stands']
+        available_bikes = station_status_dic[station['name']]['bikes']
+        available_stands = station_status_dic[station['name']]['stands']
         if station['totalStands']['availabilities']['bikes'] != available_bikes or station['totalStands']['availabilities']['stands'] != available_stands:
-            station_status_list[station['name']] = {'bikes': station['totalStands']['availabilities']['bikes'], 'stands': station['totalStands']['availabilities']['stands']}
+            station_status_dic[station['name']] = {'bikes': station['totalStands']['availabilities']['bikes'], 'stands': station['totalStands']['availabilities']['stands']}
             producer.send(topic_out, station)
     except KeyError:
         available_bikes = station['totalStands']['availabilities']['bikes']
         available_stands = station['totalStands']['availabilities']['stands']
-        station_status_list[station['name']] = {'bikes': available_bikes, 'stands': available_stands}
+        station_status_dic[station['name']] = {'bikes': available_bikes, 'stands': available_stands}
         producer.send(topic_out, station)
