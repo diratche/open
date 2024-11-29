97702cfd62e7e3ea1d9063d9711b26f6f51c5284
diratche <dimitriiratchet@yahoo.fr>
2024-11-29 08:53:02 +0100

    Update ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -14,13 +14,14 @@ except:
 
 address = server + '?apiKey=' + key
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+nested = {"user": {"id": 123, "name": "John Doe"}, "event": "login", "status": "success"}
 while True:
   data = requests.get(address)
   if data.status_code == 200:
     data = data.json()
     #producer.send(topic, value=data)
-    producer.send(topic, value='pouet')
+    producer.send(topic, value=nested)
     print(data)
-    #print(json.dumps(data).encode('utf-8'))
+    print(json.dumps(data).encode('utf-8'))
     #print(f'Ingestion successful at {time.strftime("%H:%M:%S", time.localtime())}')
   time.sleep(periodicity)
